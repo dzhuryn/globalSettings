c0752a3fc925fb2cf098f48821472fc8a2814b39
dzhuryn <dzhuryn.volodymyr@gmail.com>
2017-10-22 18:55:20 +0300

    update

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -1,5 +1,3 @@
-
-
 DROP TABLE IF EXISTS `{PREFIX}module_settings_category`;
 CREATE TABLE `{PREFIX}module_settings_category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,6 +17,5 @@ CREATE TABLE `{PREFIX}module_settings` (
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
 
 INSERT INTO `{PREFIX}module_settings_category` (`caption`) VALUES ('Без категории');
